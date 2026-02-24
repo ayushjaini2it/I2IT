ecb7353b22a3440f009ea5b4ae465db6e760bd46
ayushjaini2it <ayushj_c250107@students.isquareit.edu.in>
2026-02-24 10:09:01 +0000

    added

---
+++ isquareit/Python/filehandling.py
@@ -0,0 +1,7 @@
+file = open("Ayush.txt", 'w')
+file.write("Name: Ayush Jain\nRoll No.: FCB44\nClass: COMP-B")
+file.close
+file = open("Ayush.txt", 'r')
+# print(file.read())
+# print(file.readlines())
+print(file.readline())
---
+++ isquareit/Python/mEngapplication.py
@@ -0,0 +1,5 @@
+weight = float(input("Enter the weight of body(N): "))
+mass = float(input("Enter the mass of body(kg): "))
+
+gravity = weight / mass
+print(gravity, "m/sec^2")
