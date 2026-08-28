cbf0c2cb785c7a09e1eabc9ea9b0ec89d85420a0
ayushjaini2it <ayushj_c250107@students.isquareit.edu.in>
2026-08-28 04:44:41 +0000

    Refactor matrix operations to include user interaction and improve function signatures

--- isquareit/DSA/PD/pract2.c
+++ isquareit/DSA/PD/pract2.c
@@ -9,42 +9,107 @@
 
 void accept(int matrix[][20], int m, int n);
 
-void display(int matrix[][20], int m, int n);
+void display(int matrix[20][20], int m, int n);
 
-void add(int matrixA[20][20], int matrixB[20][20], int m, int n);
+void add(int matrixA[20][20], int matrixB[20][20], int m1, int n1, int m2, int n2);
 
 void substract(int matrixA[20][20], int matrixB[20][20], int m, int n);
 
-void multiply(int matrixA[20][20], int matrixB[20][20], int m, int n);
+void multiply(int matrixA[20][20], int matrixB[20][20], int m1, int n1, int m2, int n2);
 
 void transpose(int matrix[20][20], int m, int n);
 
 
 int main(){
 	int matrixA[20][20], matrixB[20][20];
-	int m1, n1, m2, n2;
-	printf("Enter the matrix A size:\n");
-	printf("Row size: ");
-	scanf("%d", &m1);
-	printf("Column size: ");
-	scanf("%d", &n1);
-	accept(matrixA, m1, n1);
-	printf("Enter the matrix B size:\n");
-	printf("Row size: ");
-	scanf("%d", &m2);
-	printf("Column size: ");
-	scanf("%d", &n2);
-	accept(matrixB, m2, n2);
-	display(matrixA, m1, n1);
-	display(matrixA, m1, n1);
-	add(matrixA, matrixB, m1, n1);
-	substract(matrixA, matrixB, m1, n1);
-	multiply(matrixA, matrixB, m1, n2);
+	int m1 = 0, n1 = 0, m2 = 0, n2 = 0, choice;
+	while(choice != 0){
+		printf("Enter the Operation to perform (Enter 0 to end program)\n1: Accept 2 matrices.\n2: Display a matrix.\n3: Addition of matrices\n4: Substraction of matrices.\n5: Multiplication of the matrices.\n6: Transpose of the matrix.\nChoice: ");
+		scanf("%d", &choice);
+		switch(choice){
+			case 1:{
+				printf("Enter the matrix A size:\n");
+				printf("Row size: ");
+				scanf("%d", &m1);
+				printf("Column size: ");
+				scanf("%d", &n1);
+				accept(matrixA, m1, n1);
+				printf("Enter the matrix B size:\n");
+				printf("Row size: ");
+				scanf("%d", &m2);
+				printf("Column size: ");
+				scanf("%d", &n2);
+				accept(matrixB, m2, n2);
+				break;
+			}
+			case 2:{
+				if(m1 == 0 || n1 == 0 || m2 == 0 || n2 == 0){
+					printf("No Matrix found\n");
+					break;
+				}
+				char c;
+				printf("Enter the matrix to display(A or B): ");
+				scanf(" %c", &c);
+				if(c == 'A') {
+					printf("Matrix A is:\n");
+					display(matrixA, m1, n1);
+				}
+				else if(c == 'B') {
+					printf("Matrix B is:\n");
+					display(matrixB, m2, n2);
+				}
+				else  printf("Invalid Matrix choice.");
+				break;
+			}
+			case 3:{
+				if (m1 == 0 || n1 == 0 || m2 == 0 || n2 == 0) {
+					printf("No Matrix found\n");
+					break;
+				}
+				printf("Sum of the Matrices is:\n");
+				add(matrixA, matrixB, m1, n1, m2, n2);
+				break;
+			}
+			case 4:{
+				if (m1 == 0 || n1 == 0 || m2 == 0 || n2 == 0) {
+					printf("No Matrix found\n");
+					break;
+				}
+				printf("Substraction of the Matrices is:\n");
+				substract(matrixA, matrixB, m1, n1);
+				break;
+			}
+			case 5:{
+				if (m1 == 0 || n1 == 0 || m2 == 0 || n2 == 0) {
+					printf("No Matrix found\n");
+					break;
+				}
+				printf("Multiplication of the Matrices is:\n");
+				multiply(matrixA, matrixB, m1, n1, m2, n2);
+				break;
+			}
+			case 6:{
+				if (m1 == 0 || n1 == 0 || m2 == 0 || n2 == 0) {
+					printf("No Matrix found\n");
+					break;
+				}
+				printf("Transpose of Matrix A is:\n");
+				transpose(matrixA, m1, n1);
+				break;
+			}
+			default:{
+				if(choice == 0)
+					printf("Ending program...\n");
+				else
+					printf("Invalid Choice, Try again...\n");
+				break;
+		}
+		}
+	}
 	return 0;
-}
+}	
 
-void display(int matrix[][20], int m, int n){
-	printf("The entered matrix is:\n");
+void display(int matrix[20][20], int m, int n){
 	for(int row = 0; row < m; row++){
 		for(int column = 0; column < n; column++){
 			printf("%d ", matrix[row][column]);
@@ -62,14 +127,18 @@ void accept(int matrix[20][20], int m, int n){
 	}
 }
 
-void add(int matrixA[20][20], int matrixB[20][20], int m, int n){
+void add(int matrixA[20][20], int matrixB[20][20], int m1, int n1, int m2, int n2){
+	if(m1 != m2 || n1 != n2 || m1 == 0 || n1 == 0 || m2 == 0 || n2 == 0){
+		printf("Addition is not possible(Matrices don't have same dimensions)");
+		return;
+	}
 	int matrixC[20][20];
-	for(int row = 0; row < m; row++){
-		for(int column = 0; column < n; column++){
+	for(int row = 0; row < m1; row++){
+		for(int column = 0; column < n1; column++){
 			matrixC[row][column] = matrixA[row][column] + matrixB[row][column];
 		}
 	}
-	display(matrixC, m, n);
+	display(&matrixC[0][0], m1, n1);
 }
 
 void substract(int matrixA[20][20], int matrixB[20][20], int m , int n){
@@ -82,17 +151,22 @@ void substract(int matrixA[20][20], int matrixB[20][20], int m , int n){
 	display(matrixC, m, n);
 }
 
-void multiply(int matrixA[20][20], int matrixB[20][20], int m, int n){
+void multiply(int matrixA[20][20], int matrixB[20][20], int m1, int n1, int m2, int n2){
+	if(n1 != m2 || m1 == 0 || n1 == 0 || m2 == 0 || n2 == 0){
+		printf("The Matrices Entered cannot be Multiplied (Columns of A != Rows of B)");
+		return;
+	}
 	int matrixC[20][20];
-	for(int row = 0; row < m; ++row){ 
-		for(int column = 0; column < n; ++column){
+	for(int row = 0; row < m1; ++row){ 
+		for(int column = 0; column < n1; ++column){
 			matrixC[row][column] = 0;
-			for(int i = 0; i < n; ++i){
+			for(int i = 0; i < n1; ++i){
 				matrixC[row][column] += matrixA[row][i] * matrixB[i][column];
 			}
 		}
 	}
-	display(matrixC, m, n);
+	printf("The Product of the Matrices is:\n");
+	display(&matrixC[0][0], m1, n2);
 }
 
 void transpose(int matrix[20][20], int m, int n){
@@ -102,5 +176,5 @@ void transpose(int matrix[20][20], int m, int n){
 			matrixT[column][row] = matrix[row][column];
 		}
 	}
-	display(matrixT, n, m);
+	display(&matrixT[0][0], n, m);
 }
